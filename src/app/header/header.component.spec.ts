c108a1f299f80e67349a45e25a90a4fe4b53900c
nairmanu <manunair@Manus-MacBook-Pro.local>
2018-05-13 18:08:01 -0400

    unit testing

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,25 +1,48 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { Router } from '@angular/router';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterLinkDirectiveStub } from '../../testing/RouterLinkDirectiveStub';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [
+        HeaderComponent,
+        RouterLinkDirectiveStub
+      ],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
 
+  let routerLinks;
+  let linkDes;
+
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
+    linkDes = fixture.debugElement
+    .queryAll(By.directive(RouterLinkDirectiveStub));
+
+    routerLinks = linkDes.map(de => de.injector.get(RouterLinkDirectiveStub));
+    
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+      expect(component).toBeTruthy();
+      expect(routerLinks.length).toBe(2, 'should have 2 routerLinks');
   });
+
 });
--- src/app/location/location.component.spec.ts
+++ src/app/location/location.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationComponent } from './location.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('LocationComponent', () => {
   let component: LocationComponent;
@@ -8,7 +9,8 @@ describe('LocationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LocationComponent ]
+      declarations: [ LocationComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
