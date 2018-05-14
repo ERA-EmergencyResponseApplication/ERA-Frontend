338dfa2a53ccdaa73c50ac018e2963d402de0239
logan1388 <shivs@Shivs-MacBook-Pro.local>
2018-05-13 23:18:33 -0400

    emergency page update

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-location',
@@ -9,6 +12,12 @@ export class LocationComponent implements OnInit {
   lat: any;
   lng: any;
 
+  filteredAreas: any[];
+  areas: string[] = ['Cincinnati','West Chester','Dayton','Fairfield','Cliffton'];
+  area: string;
+  emergencies: any[];
+  types: SelectItem[];
+
   constructor() {
     if(navigator){
       navigator.geolocation.getCurrentPosition(pos => {
@@ -16,7 +25,29 @@ export class LocationComponent implements OnInit {
         this.lat = +pos.coords.latitude;
       });
     }
-   }
+
+    this.types = [
+      {label: 'Paypal', value: 'PayPal', icon: 'fa fa-fw fa-cc-paypal'},
+      {label: 'Visa', value: 'Visa', icon: 'fa fa-fw fa-cc-visa'},
+      {label: 'MasterCard', value: 'MasterCard', icon: 'fa fa-fw fa-cc-mastercard'}
+  ];
+
+    this.emergencies = [
+      {name: 'Medical', code: 'Med'},
+      {name: 'Fire Hazard', code: 'Fire'},
+      {name: 'Public Safety', code: 'public'}
+    ];
+  }
+
+  filterAreas(event) {
+    this.filteredAreas = [];
+    for(let i = 0; i < this.areas.length; i++) {
+      let area = this.areas[i];
+      if(area.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
+          this.filteredAreas.push(area);
+      }
+    }
+  }
 
   ngOnInit() {
   }
