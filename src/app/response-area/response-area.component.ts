f5157b8eb30447acbdf20a93b202e6e418e59a1a
Shiv Raj Suresh <shiv-raj.suresh@us.sogeti.com>
2018-05-15 16:51:27 -0400

    Add Response area UI

--- src/app/response-area/response-area.component.ts
+++ src/app/response-area/response-area.component.ts
@@ -7,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponseAreaComponent implements OnInit {
 
-  constructor() { }
+  missingArea: string;
+  missingZip: string;
+  area: string;
+  zip: string;
+  AlertMsg: string;
+  ResAreaSuccess: boolean;
+  success: boolean;
+
+  constructor() {
+    this.ResAreaSuccess = false;
+    this.success = false;
+  }
 
   AddResArea() {
+    if (this.fieldsValid()) {
+      this.success = true;
+      this.ResAreaSuccess = true;
+      this.AlertMsg = 'Response Area added successfully!';
+    }
+  }
 
+  fieldsValid() {
+    let nv = 1;
+    this.missingArea = '';
+    this.missingZip = '';
+    if (this.area == null) {
+      nv = 0;
+      this.missingArea = 'Response Area required';
+    }
+    if (this.zip == null) {
+      nv = 0;
+      this.missingZip = 'Zip code required';
+    }
+    return nv;
   }
 
   ngOnInit() {
