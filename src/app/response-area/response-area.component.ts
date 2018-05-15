4be41159266a2e24795892ecc7010fbc1ac451ff
Shiv Raj Suresh <shiv-raj.suresh@us.sogeti.com>
2018-05-15 16:31:54 -0400

    add response area initial commit

---
+++ src/app/response-area/response-area.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-response-area',
+  templateUrl: './response-area.component.html',
+  styleUrls: ['./response-area.component.css']
+})
+export class ResponseAreaComponent implements OnInit {
+
+  constructor() { }
+
+  AddResArea() {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
