b67a03df1879f76e1411cb894fdbea4376f18715
U-CORP\MNair <MNair@TYLTCNU238B0R0.corp.tcc.inet>
2018-05-15 22:40:59 -0400

    update login module

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  name: string;
+  password: string;
+
   constructor() { }
 
   ngOnInit() {
