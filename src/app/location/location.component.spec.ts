c41da07aa04060bcc8209e35a715fb913e2702d6
nairmanu <nairmanu@users.noreply.github.com>
2018-05-15 22:25:49 -0400

    Fix Some failing Unit Tests (#27)

--- src/app/location/location.component.spec.ts
+++ src/app/location/location.component.spec.ts
@@ -2,14 +2,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationComponent } from './location.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ResponseAreaService } from '../dashboard/ResponseArea.service';
 
 describe('LocationComponent', () => {
   let component: LocationComponent;
   let fixture: ComponentFixture<LocationComponent>;
+  let responseAreaServiceStub: Partial<ResponseAreaService>;
+
 
   beforeEach(async(() => {
+    // stub ResponseAreaService for test purposes
+    responseAreaServiceStub = {
+      responseAreas: []
+    };
+
     TestBed.configureTestingModule({
       declarations: [ LocationComponent ],
+      providers:    [ {provide: ResponseAreaService, useValue: responseAreaServiceStub } ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
