334bd7b67c48bf13c748093a95965f3463b3de8d
nairmanu <nairmanu@users.noreply.github.com>
2018-05-18 00:34:46 -0400

    Add Service Test Stubs (#37)
    * disable testing to test ghost inspector
    
    * Add Service Stubs
    
    * remove fit

--- src/app/dashboard/emergencies/emergencies.component.spec.ts
+++ src/app/dashboard/emergencies/emergencies.component.spec.ts
@@ -31,7 +31,7 @@ describe('EmergenciesComponent', () => {
     esSpy = fixture.debugElement.injector.get(EmergenciesService) as any;
     component = fixture.componentInstance;
     const responseArea = new ResponseArea('Liberty center', '72.5', '74.5', 'Liberty center mall in ohio',
-    new Responder('123-123-1234', 'first 1', 'last 1', []));
+    new Responder('First', 'Last', 'username', 'Cincinnati', 'abc@xyz.com', '111-111-1112' ));
     component.respArea = responseArea;
     fixture.detectChanges();
   });
