6e0c4c3697124dcebc0a7675e0054801b3c48096
U-CORP\MNair <MNair@TYLTCNU238B0R0.corp.tcc.inet>
2018-05-18 00:22:41 -0400

    Add Service Stubs

--- src/app/location/location.component.spec.ts
+++ src/app/location/location.component.spec.ts
@@ -2,35 +2,38 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LocationComponent } from './location.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ResponseAreaServiceSpy } from '../../testing/ResponseAreaServiceSpy';
 import { ResponseAreaService } from '../dashboard/ResponseArea.service';
 
 describe('LocationComponent', () => {
   let component: LocationComponent;
   let fixture: ComponentFixture<LocationComponent>;
-  let responseAreaServiceStub: Partial<ResponseAreaService>;
-
+  let rasSpy: ResponseAreaServiceSpy;
 
   beforeEach(async(() => {
-    // stub ResponseAreaService for test purposes
-    responseAreaServiceStub = {
-      responseAreas: []
-    };
-
     TestBed.configureTestingModule({
       declarations: [ LocationComponent ],
-      providers:    [ {provide: ResponseAreaService, useValue: responseAreaServiceStub } ],
       schemas: [ NO_ERRORS_SCHEMA ]
+    })
+    .overrideComponent(LocationComponent, {
+      set: {
+        providers: [
+          { provide: ResponseAreaService, useClass: ResponseAreaServiceSpy }
+        ]
+      }
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LocationComponent);
+    rasSpy = fixture.debugElement.injector.get(ResponseAreaService) as any;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(rasSpy.getResponseAreas.calls.count()).toBe(1, 'getResponseAreas called once');
   });
 });
