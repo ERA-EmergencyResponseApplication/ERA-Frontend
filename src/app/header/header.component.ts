3b5c2591c0936385c81ae3c726793e6e380cf83c
nairmanu <nairmanu@users.noreply.github.com>
2018-05-18 14:12:19 -0400

    login css and api (#42)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Component({
   selector: 'app-header',
@@ -9,12 +15,25 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   display = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
+  private loginUrl = 'https://eraapi.brandoncodes.com/api/Responders/login';
+  email: string;
+  password: string;
 
   ngOnInit() {
   }
 
   showDialog() {
     this.display = true;
+  }
+
+  login() {
+    console.log('login');
+    this.http.post(this.loginUrl, { email: this.email, password: this.password })
+      .subscribe((response: any) => {
+        if (response.id !== null) {
+          this.display = false;
+        }
+      });
   }
 }
