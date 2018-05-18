bc2f9511ce0c57fddb96edfd96b31794d76d455f
Shiv Raj Suresh <shiv-raj.suresh@us.sogeti.com>
2018-05-18 11:47:12 -0400

    Response area UI modified as per backend model

--- src/app/dashboard/ResponseArea.ts
+++ src/app/dashboard/ResponseArea.ts
@@ -2,34 +2,42 @@ import { Emergency } from './emergencies/emergency/Emergency';
 import { Responder } from './Responder';
 
 export class ResponseArea {
-    name: string;
-    lat: any;
-    lon: any;
-    description: string;
-    id: number;
-    emergencies: Emergency[] = [];
-    ownedBy: Responder;
-    admins: Responder[] = [];
-    subscribers: Responder[] = [];
+  name: string;
+  lat: any;
+  lon: any;
+  description: string;
+  id: number;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  emergencies: Emergency[] = [];
+  ownedBy: Responder;
+  admins: Responder[] = [];
+  subscribers: Responder[] = [];
 
-    constructor(name: string, lat: any, lon: any, description: string, owner: Responder, id: number) {
-        this.name = name;
-        this.lat = lat;
-        this.lon = lon;
-        this.description = description;
-        this.ownedBy = owner;
-        this.id = id;
-    }
+  constructor(name: string, lat: any, lon: any, description: string, address: string, city: string, state: string, zip: string, owner: Responder, id: number) {
+    this.name = name;
+    this.lat = lat;
+    this.lon = lon;
+    this.description = description;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.ownedBy = owner;
+    this.id = id;
+  }
 
-    public addAdminToEmergency(responder: Responder) {
-        this.admins.push(responder);
-    }
+  public addAdminToEmergency(responder: Responder) {
+    this.admins.push(responder);
+  }
 
-    public addSubscriberToEmergency(responder: Responder) {
-        this.subscribers.push(responder);
-    }
+  public addSubscriberToEmergency(responder: Responder) {
+    this.subscribers.push(responder);
+  }
 
-    public addEmergency( emergency: Emergency ) {
-        this.emergencies.push(emergency);
-    }
+  public addEmergency( emergency: Emergency ) {
+    this.emergencies.push(emergency);
+  }
 }
--- src/testing/EmergenciesServiceSpy.ts
+++ src/testing/EmergenciesServiceSpy.ts
@@ -4,7 +4,7 @@ import { Emergency } from '../app/dashboard/emergencies/emergency/Emergency';
 import { asyncData } from './async-observable-helpers';
 
 export class EmergenciesServiceSpy {
-  responseArea = new ResponseArea('Liberty center', '72.5', '74.5', 'Liberty center mall in ohio',
+  responseArea = new ResponseArea('Liberty center', '72.5', '74.5', 'Liberty center mall in ohio', 'Address1', 'City1', 'State1', 'Zip1',
   new Responder('First', 'Last', 'username', 'Cincinnati', 'abc@xyz.com', '111-111-1112'), 1);
 
   emergency = new Emergency('72.5', '74.5', 'Medical', 'West Chester', new Date('2017-10-10'));
