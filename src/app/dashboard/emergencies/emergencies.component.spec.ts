6a5bd5681a3c362c789f9e0182a8cd8bc4b2c632
U-CORP\MNair <MNair@TYLTCNU238B0R0.corp.tcc.inet>
2018-05-18 00:31:47 -0400

    remove fit

--- src/app/dashboard/emergencies/emergencies.component.spec.ts
+++ src/app/dashboard/emergencies/emergencies.component.spec.ts
@@ -31,12 +31,12 @@ describe('EmergenciesComponent', () => {
     esSpy = fixture.debugElement.injector.get(EmergenciesService) as any;
     component = fixture.componentInstance;
     const responseArea = new ResponseArea('Liberty center', '72.5', '74.5', 'Liberty center mall in ohio',
-    new Responder('123-123-1234', 'first 1', 'last 1', []));
+    new Responder('First', 'Last', 'username', 'Cincinnati', 'abc@xyz.com', '111-111-1112' ));
     component.respArea = responseArea;
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
     // expect(esSpy.getEmergencies.calls.count()).toBe(1, 'getEmergencies called once');
   });
--- src/testing/EmergenciesServiceSpy.ts
+++ src/testing/EmergenciesServiceSpy.ts
@@ -5,7 +5,7 @@ import { asyncData } from './async-observable-helpers';
 
 export class EmergenciesServiceSpy {
   responseArea = new ResponseArea('Liberty center', '72.5', '74.5', 'Liberty center mall in ohio',
-  new Responder('123-123-1234', 'first 1', 'last 1', []));
+  new Responder('First', 'Last', 'username', 'Cincinnati', 'abc@xyz.com', '111-111-1112'));
 
   emergency = new Emergency('72.5', '74.5', 'Medical', 'West Chester', new Date('2017-10-10'));
   emergencies: Emergency[] = [this.emergency];
