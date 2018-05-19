da3d69dbdb32fdd9e610d695d459bb00d9462613
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2018-05-19 16:33:16 -0400

    Unit tests setup

--- src/app/signup/signup.component.spec.ts
+++ src/app/signup/signup.component.spec.ts
@@ -1,6 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignupComponent } from './signup.component';
+// import { SignupComponent } from './signup.component';
+// import { Responder } from '../dashboard/Responder';
+// class MockAuthService {
+//   signup(user: Responder) {
+//     return {
+//       "cellNumber": "111-111-2222",
+//       "firstName": "test",
+//       "lastName": "name",
+//       "realm": "",
+//       "username": "",
+//       "email": "test@testemail.com",
+//       "emailVerified": false,
+//       "id": 8
+//     }
+//   }
+// }
+
+// class MockRouterService {
+//   navigate(routeArray: string[]) {
+//     return true;
+//   }
+// }
+
+// class MockValidationService {
+  
+// }
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
@@ -22,4 +47,16 @@ describe('SignupComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('calls the signup API', () => {
+  //   component.isFormValid = true;
+  //   component.firstName = 'test';
+  //   component.lastName = 'test';
+  //   component.email = '';
+  //   component.phone = '555-555-5555';
+  //   component.password = 'abc123';
+  //   component.confirmPassword = 'abc123';
+  //   component.signup();
+  //   expect(component.isFormValid).toBeFalsy();
+  // });
 });
