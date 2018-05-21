54108b924f78f1eb3bb66531b544a4aee3c2d637
Shiv Raj Suresh <shiv1988@gmail.com>
2018-05-21 11:55:30 -0400

    Userdd (#61)
    * success msg
    
    * logout service call
    
    * username and password field added

--- src/app/dashboard/Responder.ts
+++ src/app/dashboard/Responder.ts
@@ -4,15 +4,25 @@ export class Responder {
   cellNumber: string;
   firstName: string;
   lastName: string;
+  userName: string;
   email: string;
   // responseAreas: any[];
   responseAreas: ResponseArea[] = [];
   password: string;
 
 
-  constructor(firstName: string, lastName: string, password: string, responseAreas: any, email: string, cellNumber: string) {
+  constructor(
+    firstName: string,
+    lastName: string,
+    userName: string,
+    password: string,
+    responseAreas: any,
+    email: string,
+    cellNumber: string
+  ) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.userName = userName;
     this.email = email;
     this.cellNumber = cellNumber;
     this.responseAreas = responseAreas;
