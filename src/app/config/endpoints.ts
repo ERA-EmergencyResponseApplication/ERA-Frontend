a2911b9d46aabdf5ce899f2dafc294ff54b24926
U-CORP\MNair <MNair@TYLTCNU238B0R0.corp.tcc.inet>
2018-05-21 21:57:09 -0400

    logout api call

--- src/app/config/endpoints.ts
+++ src/app/config/endpoints.ts
@@ -3,6 +3,7 @@ import { environment } from '../../environments/environment';
 export default {
   login: () => `${environment.url}/Responders/login`,
   signup: () => `${environment.url}/Responders`,
+  signout: () => `${environment.url}/Responders/logout`,
   createResponseArea: () => `${environment.url}/ResponseAreas`,
   getResponseAreas: () => `${environment.url}/ResponseAreas`,
   getEmergencies: (respAreaId: number) => `${environment.url}/ResponseAreas/${respAreaId}/emergencies`,
